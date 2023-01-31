1cc8f5b9238ea30c44459856413619b70aee96c3
Dimitris <dimarabatzis@outlook.com>
2023-01-31 13:29:12 +0200

    Finished 3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -26,18 +26,37 @@ const persons = [
   }
 ]
 
-// GET ALL PERSONS
-app.get('/api/persons', (req, res) => {
-  console.log('inside get all persons functionality')
-  res.json(persons)
-})
 
+
+// GET INFO
 app.get('/info', (req, res) => {
   const date = new Date()
   res.send(`
   <p>Phonebook has info for ${persons.length} people</p>
   <p>${date}</p>
   `)
+})
+
+// GET ALL PERSONS
+app.get('/api/persons', (req, res) => {
+  console.log('inside get all persons functionality')
+  res.json(persons)
+})
+
+// GET PERSON
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => { 
+    return p.id === id
+  })
+
+  if (!person) {
+    return res.status(404).json({
+      error: `Person with the id of ${id} does not exist`
+    })
+  }
+
+  res.json(person)
 })
 
 const PORT = 3001
