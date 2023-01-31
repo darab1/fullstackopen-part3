9e7b217f9b6bbe2c83611fe0ba196426fdc90e94
Dimitris <dimarabatzis@outlook.com>
2023-01-31 18:05:16 +0200

    Finished 3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -3,27 +3,28 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :body'));
 
 let persons = [
-  { 
+  {
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
@@ -49,7 +50,7 @@ app.get('/api/persons', (req, res) => {
 // GET PERSON
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  const person = persons.find(p => { 
+  const person = persons.find(p => {
     return p.id === id
   })
 
